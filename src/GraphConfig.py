e61c26ece819625aabb0b367cbc087e1c1e33cf0
Illuminubby <illuminubby@gmail.com>
2022-11-15 20:16:39 -0500

    store configuration details for graphs in GraphConfig object, including bounds

--- src/GraphConfig.py
+++ src/GraphConfig.py
@@ -1,9 +1,9 @@
-
+import BoundsManager
 
 class GraphConfig:
     
     def __init__(self, bounds, resolution):
-        self.bounds = bounds
+        self.bounds = BoundsManager.GraphBorder(bounds[0], bounds[1], bounds[2], bounds[3])
         self.resolution = resolution
         self.x = self.resolution[0]
         self.y = self.resolution[1]
