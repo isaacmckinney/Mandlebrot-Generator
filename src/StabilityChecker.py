5d7ece4c26b27dc085fe40c1507b5592f614b8f7
Illuminubby <illuminubby@gmail.com>
2022-11-15 20:18:00 -0500

    compare copy of original complex number in order to prevent issues involving changed references

--- src/StabilityChecker.py
+++ src/StabilityChecker.py
@@ -1,13 +1,13 @@
 
 
-def checkStability(points, z):
+def checkStability(points, z, steps):
     stabilityList = []
     for p in points:
         cnum = complex(p[0], p[1])
-        cnum_tracker = cnum
+        cnum_tracker = complex(p[0], p[1])
         i = 0
         boundsTracker = False
-        while(i < 500 and not boundsTracker):
+        while(i < steps and not boundsTracker):
             cnum_tracker = (cnum_tracker**z) + cnum
             if cnum_tracker.real > 5 or cnum_tracker.imag > 5 or cnum_tracker.real < -5 or cnum_tracker.imag < -5:
                 boundsTracker = True
