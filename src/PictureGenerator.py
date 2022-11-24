bdffda6e16e787a0107246d0d8f7c466a52dd01e
Illuminubby <illuminubby@gmail.com>
2022-11-24 00:21:07 -0500

    added handling functions for new coloring schemes and changed format for sylyzed array

--- src/PictureGenerator.py
+++ src/PictureGenerator.py
@@ -28,38 +28,57 @@ class PictureGenerator:
         self.styleProfile.generateRepeatedPattern(pattern, self.gradientSize)
         return self.styleProfile
 
+    def configRepeatedInterp(self, color1, color2, steps):
+        self.styleProfile = StabilityStyle(self.gradientSize)
+        self.styleProfile.generateRepeatedInterpolation(color1, color2, steps)
+        return self.styleProfile
+
+    def configInterpSequence(self, colorsToUse, steps):
+        self.styleProfile = StabilityStyle(self.gradientSize)
+        
+        self.styleProfile.generateInterpolationSequence(colorsToUse, steps)
+        return self.styleProfile
+
     def generateCnums(self):
         newCnums = pp.PixelPlot(self.gconfig).createPointsList()
         self.cnums = newCnums
 
         return self.cnums
     
-    def generateStabilityScores(self):
-        newScores = StabilityChecker.checkStability(self.cnums, 2, self.gradientSize)
+    def generateStabilityScores(self, factor):
+        newScores = StabilityChecker.checkStability(self.cnums, factor, self.gradientSize)
         self.stability = newScores
 
         return self.stability
     
     def generateStylizedArray(self):
-        stylizedArray = np.zeros((len(self.stability), 4), dtype=np.uint8)
-        for score in range(len(self.stability)):
-            #if self.styleProfile.style[ self.stability[ score ] ][0] < 253:
-                #print(self.styleProfile.style[ self.stability[ score ] ][0])
-            stylizedArray[score, 0] = (np.uint8) (self.styleProfile.style[ self.stability[ score ] ][0])
-            stylizedArray[score, 1] = (np.uint8) (self.styleProfile.style[ self.stability[ score ] ][1])
-            stylizedArray[score, 2] = (np.uint8) (self.styleProfile.style[ self.stability[ score ] ][2])
-            stylizedArray[score, 3] = (np.uint8) (self.styleProfile.style[ self.stability[ score ] ][3])
+        stylizedArray = np.zeros((self.gconfig.y, self.gconfig.x, 4), dtype=np.uint8)
 
-        stylizedArray = np.reshape( stylizedArray, (self.gconfig.x, self.gconfig.y, 4) )
-        #stylizedArray = np.transpose(stylizedArray)
+        stabCounter = 0
+
+        for row in range(self.gconfig.x):
+
+            for px in range(self.gconfig.y):
+                #print(stabCounter)
+                #print(self.stability[ stabCounter ])
+                #print(self.styleProfile.style[ self.stability[ stabCounter ] ])
+                stylizedArray[px, row, 0] = (np.uint8) (self.styleProfile.style[ self.stability[ stabCounter ] ][0])
+                stylizedArray[px, row, 1] = (np.uint8) (self.styleProfile.style[ self.stability[ stabCounter ] ][1])
+                stylizedArray[px, row, 2] = (np.uint8) (self.styleProfile.style[ self.stability[ stabCounter ] ][2])
+                stylizedArray[px, row, 3] = (np.uint8) (self.styleProfile.style[ self.stability[ stabCounter ] ][3])
+
+                stabCounter = stabCounter + 1
+
+
         self.stylizedArray = stylizedArray
         return stylizedArray
     
-    def generatePictureFromStylizedArray(self, nameOfFile, saveStatus):
+    def generatePictureFromStylizedArray(self, nameOfFile, saveStatus, showStatus):
         im = Image.fromarray(self.stylizedArray, mode="RGBA")
         self.image = im
         #self.image.transpose(method=Image.FLIP_TOP_BOTTOM)
-        self.image.show(title=nameOfFile)
+        if showStatus:
+            self.image.show(title=nameOfFile)
         if saveStatus:
             self.image.save(nameOfFile + '.png')
 
